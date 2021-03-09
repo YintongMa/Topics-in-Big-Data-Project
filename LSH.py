3bd3487acc0af977b64ec1f80cac316326ebf46e
gongyue <1024344030@qq.com>
2021-03-09 17:52:55 -0500

    add lsh ensemble & persist minhash signatures

--- LSH.py
+++ LSH.py
@@ -1,18 +1,54 @@
-from datasketch import MinHash, MinHashLSH
+from datasketch import MinHashLSHEnsemble, MinHash, MinHashLSH
+
+from dataLoader import DataLoader
+import numpy as np
+
+
 
 class LSH:
-    def __init__(self, cols, threshold, perm):
+    def __init__(self, cols, threshold, perm, path):
         self.cols = cols
+        self.size = len(cols)
         self.threshold = threshold
         self.perm = perm
+        self.path = path
+        self.mh_sigs = None
 
-    def build_index(self):
+    def build_lsh_index(self):
         lsh = MinHashLSH(threshold=self.threshold, num_perm=self.perm)
-        for i, col in enumerate(self.cols):
+        for i, sig in enumerate(self.mh_sigs):
             key = str(i)
-            mh = self.build_mh_sig(col)
-            lsh.insert(key, mh)
+            mh_obj = MinHash(num_perm=self.perm)
+            mh_array = np.asarray(sig, dtype=int)
+            mh_obj.hashvalues = mh_array
+            lsh.insert(key, mh_obj)
         return lsh
+
+    def build_lsh_ensemble_index(self):
+        lsh_ensemble = MinHashLSHEnsemble(threshold=self.threshold, num_perm=self.perm, num_part=32)
+        objects = []
+        for i, sig in enumerate(self.mh_sigs):
+            key = str(i)
+            mh_obj = MinHash(num_perm=self.perm)
+            mh_array = np.asarray(sig, dtype=int)
+            mh_obj.hashvalues = mh_array
+            objects.append((key, mh_obj, len(self.cols[i])))
+        lsh_ensemble.index(objects)
+        return lsh_ensemble
+
+    def load_sigs(self):
+        loader = DataLoader(self.path)
+        self.mh_sigs = loader.load_data()
+
+    def build_all_mh_sig(self):
+        '''
+        build minhash signatures and persist to a file
+        :return:
+        '''
+        f = open(self.path, 'w')
+        for set in self.cols:
+            mh = self.build_mh_sig(set)
+            f.write(str(mh.hashvalues.tolist()) + '\n')
 
     def build_mh_sig(self, set):
         m = MinHash(num_perm=self.perm)
@@ -20,3 +56,16 @@ class LSH:
             m.update(str(x).encode('utf8'))
         return m
 
+    def build_mh_sig_from_hashvalues(self,sig):
+        mh_obj = MinHash(num_perm=self.perm)
+        mh_array = np.asarray(sig, dtype=int)
+        mh_obj.hashvalues = mh_array
+        return mh_obj
+
+if __name__ == '__main__':
+    loader = DataLoader('columns.txt')
+    cols = loader.load_data()
+    threshold = 0.6
+    perm = 128
+    lsh = LSH(cols, threshold, perm, 'mh_sig.txt')
+    lsh.build_all_mh_sig()
