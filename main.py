38d8db79fe6aa4f6274784aae5648d20d68d3990
gongyue <1024344030@qq.com>
2021-03-08 23:49:01 -0500

    add lsh & a statistics calculator

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dataLoader import DataLoader
 from utils import DataGenerator
 from bloom_filter import BloomFilter
-
+from LSH import LSH
 
 def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in lst2]
@@ -47,11 +47,29 @@ def bloom_filter(candidate_index, cols, threshold, n, p):
 
 
 def lsh(candidate_index, cols, threshold):
-    pass
+    lsh = LSH(cols, threshold, 128)
+    index = lsh.build_index()
+    neighbors = index.query(lsh.build_mh_sig(cols[candidate_index]))
+    res = [False for _ in range(len(cols))]
+    for i in neighbors:
+        res[int(i)] = True
+    return res
 
-
-def false_positive(res, baseline):
-    pass
+def get_statistics(res, ground_truth):
+    TP, TN, FP, FN = 0, 0, 0, 0
+    for i, x in enumerate(res):
+        if x == True and ground_truth[i] == True:
+            TP += 1
+        elif x == False and ground_truth[i] == False:
+            TN += 1
+        elif x == True and ground_truth[i] == False:
+            FP += 1
+        else:
+            FN += 1
+    precision = TP/(TP + FP)
+    recall = TP/(TP + FN)
+    f1 = 2*precision*recall/(precision + recall)
+    return round(precision, 3), round(recall, 3), round(f1, 3)
 
 
 # Press the green button in the gutter to run the script.
@@ -67,25 +85,32 @@ if __name__ == '__main__':
     # show cols
     print('num of columns', len(cols))
 
-    import numpy as np
+    # import numpy as np
 
+    threshold = 0.6
+    candidate_index = 166
     # brute_force
-    brute_force_result = brute_force(166, cols, 0.6)
-    print("brute_force_result:")
-    print(brute_force_result)
+    brute_force_result = brute_force(candidate_index, cols, threshold)
+    print("brute_force finished")
 
     # bloom filter
     block_cnt = 20
     block_len = 30
     n = block_cnt * block_len  # code space. set it to the max size of a col for now
     p = 0.01  # false positive probability
-    bloom_filter_result = bloom_filter(166, cols, threshold=0.6, n=n, p=p)
-    print("bloom_filter_result:")
-    print(bloom_filter_result)
+    bloom_filter_result = bloom_filter(candidate_index, cols, threshold=threshold, n=n, p=p)
+    print("bloom_filter finished")
 
-    brute_force_np = np.array(brute_force_result, dtype=bool)
-    bloom_filter_np = np.array(bloom_filter_result, dtype=bool)
+    precision, recall, f1 = get_statistics(bloom_filter_result, brute_force_result)
+    print(precision, recall, f1)
 
-    correct = (brute_force_np == bloom_filter_np)
-    accuracy = correct.sum() / correct.size
-    print("accuracy of bloom filter = " + str(accuracy))
+    lsh_result = lsh(candidate_index, cols, threshold)
+    print("lsh finished")
+    precision, recall, f1 = get_statistics(lsh_result, brute_force_result)
+    print(precision, recall, f1)
+    # brute_force_np = np.array(brute_force_result, dtype=bool)
+    # bloom_filter_np = np.array(bloom_filter_result, dtype=bool)
+    #
+    # correct = (brute_force_np == bloom_filter_np)
+    # accuracy = correct.sum() / correct.size
+    # print("accuracy of bloom filter = " + str(accuracy))
