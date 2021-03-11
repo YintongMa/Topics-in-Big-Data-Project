0220eff1ca00710bf7da6e8afebeefb33e4e8d90
youny626 <zhiru@uchicago.edu>
2021-03-11 15:20:37 +0800

    add serialize/deserialize bloom filters

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dataLoader import DataLoader
 from bloom_filter import BloomFilter
 from LSH import LSH
 import time
+from bitarray import bitarray
 
 
 def intersection(lst1, lst2):
@@ -23,16 +24,8 @@ def brute_force(candidate_index, cols, threshold):
     return res
 
 
-def bloom_filter(candidate_index, cols, threshold, n, p):
+def bloom_filter(candidate_index, cols, threshold, bloom_filter_list):
     res = [False for _ in range(len(cols))]
-
-    # build bloom filter for all cols
-    bloom_filter_list = []
-    for col in cols:
-        bloom_filter = BloomFilter(n, p)
-        for num in col:
-            bloom_filter.add(chr(num))
-        bloom_filter_list.append(bloom_filter)
 
     start = time.time()
     for i in range(len(cols)):
@@ -78,19 +71,10 @@ def lsh_ensemble(candidate_index, lsh, threshold):
     processing_time = time.time() - start
     return res, processing_time
 
-def lsh_bloom_filter(candidate_index, lsh, lsh_threshold, overlap_threshold):
+def lsh_bloom_filter(candidate_index, lsh, lsh_threshold, overlap_threshold, bloom_filter_list):
     query_mh = lsh.build_mh_sig_from_hashvalues(lsh.mh_sigs[candidate_index])
     index = lsh.build_lsh_index(lsh_threshold)
     res = [False for _ in range(lsh.size)]
-
-    # build bloom filter for all cols
-    bloom_filter_list = []
-    for col in cols:
-        bloom_filter = BloomFilter(n, p)
-        for num in col:
-            bloom_filter.add(chr(num))
-        bloom_filter_list.append(bloom_filter)
-
 
     start = time.time()
     neighbors = index.query(query_mh)
@@ -130,6 +114,40 @@ def get_statistics(res, ground_truth):
     f1 = 2 * precision * recall / (precision + recall)
     return round(precision, 3), round(recall, 3), round(f1, 3)
 
+def load_bloom_filters(dir_path, count, n, p):
+
+    bloom_filters = []
+    for i in range(count):
+        file_path = dir_path + "bloom_filter_" + str(i)
+        f = open(file_path, 'rb')
+
+        bloom_filter = BloomFilter(n, p)
+        bloom_filter.bit_array = bitarray(endian='little')
+        bloom_filter.bit_array.frombytes(f.read())
+        # print(bloom_filter.bit_array.length())
+        bloom_filters.append(bloom_filter)
+
+    # f = open(path, 'rb')
+    # # lines = f.readlines()
+    # bloom_filters = []
+    # output = f.read()
+    # bytes_list = output.split(b'\n\n')
+    # print("len(bytes_list)", len(bytes_list))
+    # for bit_array_in_bytes in bytes_list:
+    #     bloom_filter = BloomFilter(n, p)
+    #     bloom_filter.bit_array = bitarray(endian='little')
+    #     bloom_filter.bit_array.frombytes(bit_array_in_bytes)
+    #     print(bloom_filter.bit_array.length())
+    #     bloom_filters.append(bloom_filter)
+    # for line in lines:
+    #     bloom_filter = BloomFilter(n, p)
+    #     line_list = line.split(b'\n')
+    #     bit_array_in_bytes = line_list[0]
+    #     bloom_filter.bitarray = bitarray(endian='little')
+    #     bloom_filter.bit_array.frombytes(bit_array_in_bytes)
+    #     bloom_filters.append(bloom_filter)
+    return bloom_filters
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -157,14 +175,17 @@ if __name__ == '__main__':
     block_len = 30
     n = block_cnt * block_len  # code space. set it to the max size of a col for now
     p = 0.01  # false positive probability
-    bloom_filter_result, t = bloom_filter(candidate_index, cols, threshold=threshold, n=n, p=p)
+    # load bloom filters from file
+    bloom_filter_dir_path = "./bloom_filter/"
+    bloom_filter_list = load_bloom_filters(bloom_filter_dir_path, len(cols), n, p)
+    print("load bloom filters finished\n")
+
+    bloom_filter_result, t = bloom_filter(candidate_index, cols, threshold, bloom_filter_list)
     print("bloom_filter finished, used %s s" % str(round(t, 4)))
 
     precision, recall, f1 = get_statistics(bloom_filter_result, brute_force_result)
     print(precision, recall, f1, '\n')
 
-    loader = DataLoader('columns.txt')
-    cols = loader.load_data()
     lsh = LSH(cols, 128, 'mh_sig.txt')
     lsh.load_sigs()
     print("build lsh signature finished\n")
@@ -182,7 +203,7 @@ if __name__ == '__main__':
     print(precision, recall, f1, '\n')
 
     print("lsh + bloom filter")
-    res, t = lsh_bloom_filter(candidate_index, lsh, 0.1, threshold)
+    res, t = lsh_bloom_filter(candidate_index, lsh, 0.1, threshold, bloom_filter_list)
     print("lsh + bloom filter finished, used %s s" % str(round(t, 4)))
     precision, recall, f1 = get_statistics(res, brute_force_result)
     print(precision, recall, f1)
